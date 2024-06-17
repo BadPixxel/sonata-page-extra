425a99b70425ffda5e0ec953a66321f76249bef0
BadPixxel <eshop.bpaquier@gmail.com>
2024-06-17 21:27:36 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sonata Pages Extra Pack
+
+Add Extra features to Sonata Page
+
+## License
+
+This package is available under the [MIT license](LICENSE).
+
---
+++ tests/config/bundles.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ *  Copyright (C) BadPixxel <www.badpixxel.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+return array(
+    //==============================================================================
+    // SYMFONY STANDARD EDITION
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => array("all" => true),
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => array("all" => true),
+    Symfony\Bundle\TwigBundle\TwigBundle::class => array("all" => true),
+    Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle::class => array("all" => true),
+);
